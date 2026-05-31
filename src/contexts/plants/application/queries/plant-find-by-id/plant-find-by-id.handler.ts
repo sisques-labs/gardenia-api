4cbd4377586b487be7d26d7185292591fc63b545
JSisques <javierplazasisques@gmail.com>
2026-05-31 14:25:19 +0200

    feat(plant): add logging to PlantFindByIdQueryHandler
    Implemented logging in the PlantFindByIdQueryHandler to track the execution of the query, enhancing observability and debugging capabilities. This addition helps in monitoring the retrieval process of plant data by logging the plant ID being queried.

--- src/contexts/plants/application/queries/plant-find-by-id/plant-find-by-id.handler.ts
+++ src/contexts/plants/application/queries/plant-find-by-id/plant-find-by-id.handler.ts
@@ -2,6 +2,7 @@ import { PlantFindByIdQuery } from '@contexts/plants/application/queries/plant-f
 import { AssertPlantViewModelExistsService } from '@contexts/plants/application/services/read/assert-plant-view-model-exists/assert-plant-view-model-exists.service';
 import { EnrichPlantWithQrService } from '@contexts/plants/application/services/read/enrich-plant-with-qr/enrich-plant-with-qr.service';
 import { PlantViewModel } from '@contexts/plants/domain/view-models/plant.view-model';
+import { Logger } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 
 @QueryHandler(PlantFindByIdQuery)
@@ -9,15 +10,22 @@ export class PlantFindByIdQueryHandler implements IQueryHandler<
   PlantFindByIdQuery,
   PlantViewModel
 > {
+  private readonly logger = new Logger(PlantFindByIdQueryHandler.name);
+
   constructor(
     private readonly assertPlantViewModelExistsService: AssertPlantViewModelExistsService,
     private readonly enrichPlantWithQrService: EnrichPlantWithQrService,
   ) {}
 
   async execute(query: PlantFindByIdQuery): Promise<PlantViewModel> {
+    this.logger.log(
+      `Executing PlantFindByIdQuery for plant ${query.plantId.value}`,
+    );
+
     const plant = await this.assertPlantViewModelExistsService.execute(
       query.plantId,
     );
+
     return this.enrichPlantWithQrService.execute(plant);
   }
 }
