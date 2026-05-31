90ce75ba656c2bf4aed34ee8e651aaf9d9133e15
JSisques <javierplazasisques@gmail.com>
2026-05-31 14:27:13 +0200

    feat(plant): enhance logging in EnrichPlantWithQrService
    Added logging functionality to the EnrichPlantWithQrService to improve observability during the enrichment process of plants with QR codes. This includes logging the plant ID being enriched, warnings for plants without QR IDs, and debug information when a QR is found.

--- src/contexts/plants/application/services/read/enrich-plant-with-qr/enrich-plant-with-qr.service.ts
+++ src/contexts/plants/application/services/read/enrich-plant-with-qr/enrich-plant-with-qr.service.ts
@@ -1,22 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
-
+import { PlantViewModel } from '@contexts/plants/domain/view-models/plant.view-model';
 import { QrFindByIdQuery } from '@contexts/qr/application/queries/qr-find-by-id/qr-find-by-id.query';
 import { QrViewModel } from '@contexts/qr/domain/view-models/qr.view-model';
-import { PlantViewModel } from '@contexts/plants/domain/view-models/plant.view-model';
+import { Injectable, Logger } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
 
 @Injectable()
 export class EnrichPlantWithQrService {
+  private readonly logger = new Logger(EnrichPlantWithQrService.name);
+
   constructor(private readonly queryBus: QueryBus) {}
 
   async execute(plant: PlantViewModel): Promise<PlantViewModel> {
+    this.logger.log(`Enriching plant ${plant.id} with QR`);
+
     if (!plant.qrId) {
+      this.logger.warn(`Plant ${plant.id} has no QR`);
       return plant;
     }
 
     const qr = await this.queryBus.execute<QrFindByIdQuery, QrViewModel>(
       new QrFindByIdQuery({ qrId: plant.qrId }),
     );
+
+    this.logger.debug(`QR ${qr.id} found for plant ${plant.id}`);
 
     return new PlantViewModel({
       id: plant.id,
