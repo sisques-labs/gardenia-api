54f31f4c17b10dbaf9558179906e0d80b341750b
JSisques <javierplazasisques@gmail.com>
2026-06-03 09:55:16 +0200

    refactor(planting-spots): update PlantingSpotFindByIdQuery to use 'id' property
    - Changed references from 'spotId' to 'id' in PlantingSpotFindByIdQuery, its handler, and associated tests for consistency with recent refactoring efforts.
    - Updated logging and input types to align with the new property naming convention.

--- src/contexts/planting-spots/application/queries/planting-spot-find-by-id/planting-spot-find-by-id.query.ts
+++ src/contexts/planting-spots/application/queries/planting-spot-find-by-id/planting-spot-find-by-id.query.ts
@@ -1,18 +1,19 @@
 import { UuidValueObject } from '@sisques-labs/nestjs-kit';
 
+import { IPlantingSpotPrimitives } from '@contexts/planting-spots/domain/primitives/planting-spot.primitives';
 import { PlantingSpotIdValueObject } from '@contexts/planting-spots/domain/value-objects/planting-spot-id/planting-spot-id.value-object';
 
-export interface PlantingSpotFindByIdQueryInput {
-  spotId: string;
-  spaceId: string;
-}
+export type PlantingSpotFindByIdQueryInput = Pick<
+  IPlantingSpotPrimitives,
+  'id' | 'spaceId'
+>;
 
 export class PlantingSpotFindByIdQuery {
-  public readonly spotId: PlantingSpotIdValueObject;
+  public readonly id: PlantingSpotIdValueObject;
   public readonly spaceId: UuidValueObject;
 
   constructor(input: PlantingSpotFindByIdQueryInput) {
-    this.spotId = new PlantingSpotIdValueObject(input.spotId);
+    this.id = new PlantingSpotIdValueObject(input.id);
     this.spaceId = new UuidValueObject(input.spaceId);
   }
 }
