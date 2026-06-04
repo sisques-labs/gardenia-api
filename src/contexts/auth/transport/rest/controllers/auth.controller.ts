2047b7a08043120b7b1ffaa98c9fa1fc2c583663
JSisques <javierplazasisques@gmail.com>
2026-06-04 09:03:08 +0200

    Revert "fix(auth): clear refresh cookie on token rotation failure"
    This reverts commit f21df78fcfd6706964fd549972366a94c2069c86.

--- src/contexts/auth/transport/rest/controllers/auth.controller.ts
+++ src/contexts/auth/transport/rest/controllers/auth.controller.ts
@@ -139,17 +139,12 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     const refreshToken = req.cookies[REFRESH_COOKIE_NAME] as string | undefined;
     if (!refreshToken) throw new UnauthorizedException();
-    try {
-      const result = await this.commandBus.execute<
-        RefreshTokenCommand,
-        { accessToken: string; refreshToken: string }
-      >(new RefreshTokenCommand({ refreshToken }));
-      setRefreshCookie(res, result.refreshToken);
-      return { accessToken: result.accessToken };
-    } catch (error) {
-      clearRefreshCookie(res);
-      throw error;
-    }
+    const result = await this.commandBus.execute<
+      RefreshTokenCommand,
+      { accessToken: string; refreshToken: string }
+    >(new RefreshTokenCommand({ refreshToken }));
+    setRefreshCookie(res, result.refreshToken);
+    return { accessToken: result.accessToken };
   }
 
   @Post('logout')
